f8324be2c69e7b20991c9ce2c0538dfd2351366f
Sam-Dunlap <sam.dunlap15@gmail.com>
2022-05-31 20:51:21 -0700

    first commit

---
+++ dogGetter.jsx
@@ -0,0 +1,22 @@
+function App() {
+  const {Button} = ReactBootstrap;
+  const {useState, useEffect} = React;
+  const [url, setUrl] = useState('https://images.dog.ceo/breeds/pointer-german/n02100236_2974.jpg');
+
+  function handleClick() {
+    async function dogGet() {
+      let dog = await axios('https://dog.ceo/api/breeds/image/random')
+      setUrl(dog.data.message);
+    }
+    dogGet();
+  }
+
+  return (
+    <>
+      <img src={url}></img>
+      <Button onClick={handleClick}>Get Me a Dog!</Button>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
