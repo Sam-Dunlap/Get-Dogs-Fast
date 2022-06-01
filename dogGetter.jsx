45127e847e3646f2724331cb9fcba1e25db1f358
Sam-Dunlap <sam.dunlap15@gmail.com>
2022-05-31 21:25:34 -0700

    added styles

--- dogGetter.jsx
+++ dogGetter.jsx
@@ -2,20 +2,24 @@ function App() {
   const {Button} = ReactBootstrap;
   const {useState} = React;
   const [url, setUrl] = useState('https://images.dog.ceo/breeds/pointer-german/n02100236_2974.jpg');
+  const [prevDogs, setPrevDogs] = useState([]);
 
   function handleClick() {
+    setPrevDogs([...prevDogs, url]);
     async function dogGet() {
       let dog = await axios('https://dog.ceo/api/breeds/image/random')
+      console.log(dog);
       setUrl(dog.data.message);
     }
     dogGet();
   }
 
   return (
-    <>
-      <img src={url}></img>
-      <Button onClick={handleClick}>Get Me a Dog!</Button>
-    </>
+    <div className='card' style={{width: window.innerWidth / 2, margin: 'auto', backgroundColor: '#809184'}}>
+      <img className="card-img-top" style={{height: '500px'}} src={url}></img>
+      <h5 className='card-title'></h5>
+      <Button style={{backgroundColor: '#116A68'}} onClick={handleClick}>Get Me a Dog!</Button>
+    </div>
   )
 }
 
